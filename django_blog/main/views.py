3808ff552cbbc59d23767789d5bddc95ae878aee
Denys Bezsmertnyi <gerbyboyz@gmail.com>
2023-10-11 17:27:14 +0300

    create templates + mini design

--- django_blog/main/views.py
+++ django_blog/main/views.py
@@ -1,68 +1,71 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
 
 
 def show_article(request, article_id):
     return render(request, 'main/post/detail.html')
 
-def show_home_page(request):
-    return render(request, 'main/post/list.html')
+
+def show_home_with_categories(request):
+    category_list = ["Anime", "Cartoon", "Horror", "Sci-fi", "Novell"]
+    return render(request, 'main/post/list.html', {'category_list': category_list})
 
 
-def show_about(request) -> HttpResponse:
-    return HttpResponse("About text")
-
-def add_comment(request, article_id) -> HttpResponse:
-    return HttpResponse("Add comment here")
+def show_about(request):
+    return render(request, 'main/about.html')
 
 
-def create_article(request) -> HttpResponse:
-    return HttpResponse("Article create")
+def add_comment(request, article_id):
+    return render(request, 'main/post/add_comment.html')
 
 
-def update_article(request, article_id) -> HttpResponse:
-    return HttpResponse("Article update")
+def create_article(request):
+    return render(request, 'main/post/create_article.html')
 
 
-def delete_article(request, article_id) -> HttpResponse:
-    return HttpResponse("Article delete")
+def update_article(request, article_id):
+    return render(request, 'main/post/update_article.html')
 
 
-def list_topics(request) -> HttpResponse:
-    return HttpResponse("Topics list")
+def delete_article(request, article_id):
+    return render(request, 'main/post/delete_article.html')
 
 
-def subscribe_to_topic(request, topic) -> HttpResponse:
-    return HttpResponse("Subscribe to topic")
+def topic_list(request):
+    return render(request, 'main/topic/topic_list.html')
 
 
-def unsubscribe_from_topic(request, topic) -> HttpResponse:
-    return HttpResponse("Unsubscribe from topic")
+def subscribe_to_topic(request, topic):
+    return render(request, 'main/topic/topic_subscribe.html')
 
 
-def user_profile(request, username) -> HttpResponse:
-    return HttpResponse("User profile")
+def unsubscribe_from_topic(request, topic):
+    return render(request, 'main/topic/topic_unsubscribe.html')
 
 
-def set_password(request) -> HttpResponse:
-    return HttpResponse("Set password")
+def user_profile(request, username):
+    return render(request, 'main/user/profile.html')
 
 
-def set_userdata(request) -> HttpResponse:
-    return HttpResponse("Set userdata")
+def set_password(request):
+    return render(request, 'main/user/set_password.html')
 
 
-def deactivate_account(request) -> HttpResponse:
-    return HttpResponse("Account deactivate")
+def set_userdata(request):
+    return render(request, 'main/user/set_userdata.html')
 
 
-def register_user(request) -> HttpResponse:
-    return HttpResponse("Register user")
+def deactivate_account(request):
+    return render(request, 'main/user/deactivate.html')
+
+
+def register_user(request):
+    return render(request, 'main/user/register.html')
 
 
 def user_login(request) -> HttpResponse:
-    return HttpResponse("User login")
+    return render(request, 'main/user/login.html')
 
 
-def user_logout(request) -> HttpResponse:
-    return HttpResponse("User logout")
+def user_logout(request):
+    return render(request, 'main/user/logout.html')
