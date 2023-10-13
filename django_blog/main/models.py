dfec01baf2a2818ccab3133fb147eda946850882
Denys Bezsmertnyi <gerbyboyz@gmail.com>
2023-10-13 22:17:25 +0300

    add models

--- django_blog/main/models.py
+++ django_blog/main/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length=500)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey("Article", on_delete=models.CASCADE)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ManyToManyField("Topic")
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=255)
+    subscribers = models.ManyToManyField(User, through="Preference")
+
+
+class Preference(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    is_notified = models.BooleanField(default=False)
